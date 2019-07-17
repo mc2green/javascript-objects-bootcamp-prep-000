ccff79588ee54896ebb565d22be66e579178514a
Matthew Chen <mc.2green@gmail.com>
2019-07-17 17:57:10 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,5 @@
 var playlist = {Alicia Keys: "No One", Maroon 5: "She Will be Loved", Meghan Trainor: "No Excuses"};
+
+function updatePlaylist(playlist, artist, song){
+  return playlist.artist = song;
+}
