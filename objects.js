cbb553ecae8093272a9f08dffb21878064211a45
Matthew Chen <mc.2green@gmail.com>
2019-07-17 18:50:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,11 +1,14 @@
-var playlist = {'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes'};
+var playlist = {'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes'} 
 
-function updatePlaylist(playlist, artistName, songTitle){
-  Object.assign({}, playlist, {[artistName]: songTitle});
-  return playlist;
+ function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
 }
 
-function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName;
-  return playlist;
-}
+   updatePlaylist(playlist, "phil Ochs", "Here's to the State of Mississippi")
+
+ function removeFromPlaylist(playlist, artistName) {
+  delete playlist.Slowdive;
+
+   return playlist
+}  
